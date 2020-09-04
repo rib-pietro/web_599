1761d5c0d75b07a250880bc6ead4a4723755515d
Pietro Ribeiro <ribpietro@gmail.com>
2020-09-04 19:34:41 -0300

    adding javascript files

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+// const nome = window.prompt('Qual seu nome?');
+
+// console.log(nome);
+
+// document.write('Meu primeiro JS');
+
+const p = document.querySelector('p');
+
+// p.innerText = 'Atribuído via JS';
+
+// p.innerHTML = '<strong>Forte</strong>';
+
+function hello() {
+    alert('Fui clicado!');
+}
