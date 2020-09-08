091fa029e1f06368d853ec691de7e7e78b3b7eef
Pietro Ribeiro <ribpietro@gmail.com>
2020-09-08 20:33:25 -0300

    aula 08/09 pré-intervalo

--- javascript/script.js
+++ javascript/script.js
@@ -11,5 +11,57 @@ const p = document.querySelector('p');
 // p.innerHTML = '<strong>Forte</strong>';
 
 function hello() {
-    alert('Fui clicado!');
+    alert('Alerta!!!!');
 }
+
+// const funcao = function () { // função anônima
+//     alert('Alerta!!!!');
+// };
+
+// const funcao = () => { // arrow function
+//     alert('Alerta!!!!');
+// };
+
+// funcao();
+
+// function trackMouse($event) {
+//     console.log(`x: ${$event.clientX}
+// y: ${$event.clientY}`);
+// }
+
+// console.log(trackMouse);
+
+// const infoPietro = {
+//     'nome': 'Pietro',
+//     'sobrenome': 'Ribeiro',
+//     idade: 25,
+//     'maior de idade': true,
+//     pai: {
+//         nome: 'Evandro',
+//         sobrenome: 'Ribeiro',
+//     },
+//     minhaFuncao: function () { // função anônima
+//         alert('Alerta!!!!');
+//     }
+// };
+
+// infoPietro['temFilhos'] = false;
+
+// console.log(infoPietro);
+
+// infoPietro.minhaFuncao();
+
+
+
+
+// function somar(a, b) {
+//     return a + b;
+// }
+
+const somar = (a, b) => {
+    const soma = a + b;
+    return soma;
+}
+const subtrair
+
+console.log(somar(2, 2.5));
